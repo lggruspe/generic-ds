a0c7d009135acec5eba9257f6e413a32ca9aa2d4
lggruspe <mail.levig@gmail.com>
2019-07-07 23:28:12 +0800

    Implement tree rotations

--- red-black-tree/rbtree.h
+++ red-black-tree/rbtree.h
@@ -29,10 +29,56 @@ rb_node_t *rb_node_create(
 
 rb_node_t *rb_node_search(rb_node_t *root, int key)
 {
-
+    rbnode_t *node = root;
+    while (node && node->key != key) {
+        node = (key < node->key ? node->left : node->right);
+    }
+    return node;
 }
 
+// should only be called if x and y are not null
 void rb_left_rotate(rb_node_t *x, rb_node_t *y)
 {
+    if (!x || !y) {
+        exit(EXIT_FAILURE);
+    }
+    y->parent = x->parent;
+    if (y->parent) {
+        if (y->parent->left == x) {
+            y->parent->left = y;
+        } else {
+            y->parent->right = y;
+        }
+    }
 
+    x->right = y->left;
+    if (x->right) {
+        x->right->parent = x;
+    }
+
+    y->left = x;
+    x->parent = y;
+}
+
+void rb_right_rotate(rb_node_t *x, rb_node_t *y)
+{
+    if (!x || !y) {
+        exit(EXIT_FAILURE);
+    }
+    x->parent = y->parent;
+    if (x->parent) {
+        if (x->parent->left == y) {
+            x->parent->left = x;
+        } else {
+            x->parent->right = x;
+        }
+    }
+
+    y->left = x->right;
+    if (y->left) {
+        y->left->parent = y;
+    }
+
+    x->right = y;
+    y->parent = x;
 }
