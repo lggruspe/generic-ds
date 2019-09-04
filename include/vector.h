3fc1268215f0b1628cd798786ede6234626907f9
lggruspe <mail.levig@gmail.com>
2019-09-04 22:47:45 +0800

    Make a macro for the vector growth factor, and remove dummy member

--- include/vector.h
+++ include/vector.h
@@ -24,17 +24,26 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#define vector_growth_factor 2.0
 #define vector_register(type) \
 struct vector_##type {\
     int size;\
     int capacity;\
     type *array;\
-    type dummy;\
 };
 
+#define vector_get(vec, i) ((vec)->array[i])
+
+#define vector_set(vec, j, data) do {\
+    if ((j) < (vec)->size) {\
+        (vec)->array[(j)] = (data);\
+    }\
+} while (0)
+
 #define vector_is_empty(vector) ((vector)->size == 0)
-#define vector_create() { .array = NULL }
-#define vector_peek(vec) ((vec)->array[(vec)->size - 1])
+#define vector_is_full(vector) ((vector)->size == (vector)->capacity)
+#define vector_create() { .array = NULL, .size = 0, .capacity = 0 }
+#define vector_peek(vec) vector_get((vec), (vec)->size - 1)
 
 #define vector_pop(vec) do {\
     if (!vector_is_empty(vec)) {\
@@ -43,24 +52,20 @@ struct vector_##type {\
 } while (0)
 
 #define vector_push(vec, data) do {\
-    if ((vec)->size == (vec)->capacity) {\
-        int capacity = (vec)->capacity ? 2*(vec)->capacity : 1;\
-        void *array = realloc((vec)->array, capacity * sizeof((vec)->dummy));\
+    if (vector_is_full(vec)) {\
+        int capacity = (vec)->capacity * vector_growth_factor;\
+        if (capacity <= 0) {\
+            capacity = 1;\
+        }\
+        void *array = realloc((vec)->array, capacity * sizeof(data));\
         if (array) {\
             (vec)->array = array;\
             (vec)->capacity = capacity;\
         }\
     }\
-    if ((vec)->size != (vec)->capacity) {\
-        (vec)->array[(vec)->size++] = (data);\
-    }\
-} while (0)
-
-#define vector_get(vec, i) ((vec)->array[i])
-
-#define vector_set(vec, j, data) do {\
-    if ((j) < (vec)->size) {\
-        (vec)->array[(j)] = (data);\
+    if (!vector_is_full(vec)) {\
+        (vec)->size++;\
+        vector_set((vec), (vec)->size - 1, (data));\
     }\
 } while (0)
 
