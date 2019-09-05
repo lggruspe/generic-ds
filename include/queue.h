b3d5ecdb88d45dad5a3a26d066d3019fdb6c2e89
lggruspe <mail.levig@gmail.com>
2019-09-05 14:46:45 +0800

    Pass queue by pointer pointer instead of 'reference'

--- include/queue.h
+++ include/queue.h
@@ -14,27 +14,27 @@
 
 #define queue_create(...) {.growth_factor = 2.0, ##__VA_ARGS__}
 
-#define queue_is_empty(queue) ((queue).size == 0)
+#define queue_is_empty(queue) ((queue)->size == 0)
 
-#define queue_is_full(queue) ((queue).size >= (queue).capacity)
+#define queue_is_full(queue) ((queue)->size >= (queue)->capacity)
 
 #define queue_increase_capacity(queue) do {\
-    int new_capacity = (queue).growth_factor*(queue).capacity;\
+    int new_capacity = (queue)->growth_factor*(queue)->capacity;\
     new_capacity = new_capacity ? new_capacity : 1;\
-    void *temp = malloc(new_capacity * sizeof((queue).dummy));\
+    void *temp = malloc(new_capacity * sizeof((queue)->dummy));\
     if (temp) {\
-        if ((queue).front < (queue).back) {\
-            memcpy(temp, (queue).array, ((queue).back - (queue).front) * sizeof((queue).dummy));\
+        if ((queue)->front < (queue)->back) {\
+            memcpy(temp, (queue)->array, ((queue)->back - (queue)->front) * sizeof((queue)->dummy));\
         } else if (!queue_is_empty(queue)) {\
-            memcpy(temp, (queue).array + (queue).front, ((queue).capacity - (queue).front) * sizeof((queue).dummy));\
-            memcpy((char*)temp + ((queue).capacity - (queue).front) * sizeof((queue).dummy),\
-                    (queue).array, (queue).back * sizeof((queue).dummy));\
+            memcpy(temp, (queue)->array + (queue)->front, ((queue)->capacity - (queue)->front) * sizeof((queue)->dummy));\
+            memcpy((char*)temp + ((queue)->capacity - (queue)->front) * sizeof((queue)->dummy),\
+                    (queue)->array, (queue)->back * sizeof((queue)->dummy));\
         }\
-        free((queue).array);\
-        (queue).array = temp;\
-        (queue).capacity = new_capacity;\
-        (queue).front = 0;\
-        (queue).back = (queue).size;\
+        free((queue)->array);\
+        (queue)->array = temp;\
+        (queue)->capacity = new_capacity;\
+        (queue)->front = 0;\
+        (queue)->back = (queue)->size;\
     }\
 } while (0)
 
@@ -43,38 +43,38 @@
         queue_increase_capacity(queue);\
     }\
     if (!queue_is_full(queue)) {\
-        (queue).array[(queue).back++] = (item);\
-        if ((queue).back >= (queue).capacity) {\
-            (queue).back = 0;\
+        (queue)->array[(queue)->back++] = (item);\
+        if ((queue)->back >= (queue)->capacity) {\
+            (queue)->back = 0;\
         }\
-        (queue).size++;\
+        (queue)->size++;\
     }\
 } while (0)
 
 // returns pointer to front
-#define queue_peek(queue) (queue_is_empty(queue) ? NULL : (queue).array + (queue).front)
+#define queue_peek(queue) (queue_is_empty(queue) ? NULL : (queue)->array + (queue)->front)
 
 #define queue_dequeue(queue) do {\
     if (!queue_is_empty(queue)) {\
-        (queue).front++;\
-        if ((queue).front >= (queue).capacity) {\
-            (queue).front = 0;\
+        (queue)->front++;\
+        if ((queue)->front >= (queue)->capacity) {\
+            (queue)->front = 0;\
         }\
-        (queue).size--;\
+        (queue)->size--;\
     }\
     if (queue_is_empty(queue)) {\
-        (queue).front = 0;\
-        (queue).back = 0;\
+        (queue)->front = 0;\
+        (queue)->back = 0;\
     }\
 } while (0)
 
 #define queue_destroy(queue) do {\
-    if ((queue).array) {\
-        free((queue).array);\
-        (queue).array = NULL;\
-        (queue).size = 0;\
-        (queue).capacity = 0;\
-        (queue).front = 0;\
-        (queue).back = 0;\
+    if ((queue)->array) {\
+        free((queue)->array);\
+        (queue)->array = NULL;\
     }\
+    (queue)->size = 0;\
+    (queue)->capacity = 0;\
+    (queue)->front = 0;\
+    (queue)->back = 0;\
 } while (0)
--- test/queue.c
+++ test/queue.c
@@ -10,70 +10,70 @@ unit_test(test_queue_init)
     assert_true(queue.capacity == 0);
     assert_true(queue.front == 0);
     assert_true(queue.back == 0);
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_is_empty)
 {
     queue(char) queue = queue_create();
-    assert_true(queue_is_empty(queue));
-    queue_enqueue(queue, 0);
-    assert_true(!queue_is_empty(queue));
-    queue_dequeue(queue);
-    assert_true(queue_is_empty(queue));
-    queue_destroy(queue);
+    assert_true(queue_is_empty(&queue));
+    queue_enqueue(&queue, 0);
+    assert_true(!queue_is_empty(&queue));
+    queue_dequeue(&queue);
+    assert_true(queue_is_empty(&queue));
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_is_full)
 {
     queue(char) queue = queue_create();
-    assert_true(queue_is_full(queue));
-    queue_enqueue(queue, 1);
-    assert_true(queue_is_full(queue));
-    queue_enqueue(queue, 2);
-    assert_true(queue_is_full(queue));
-    queue_enqueue(queue, 3);
-    assert_true(!queue_is_full(queue));
-    queue_enqueue(queue, 4);
-    assert_true(queue_is_full(queue));
-    queue_destroy(queue);
+    assert_true(queue_is_full(&queue));
+    queue_enqueue(&queue, 1);
+    assert_true(queue_is_full(&queue));
+    queue_enqueue(&queue, 2);
+    assert_true(queue_is_full(&queue));
+    queue_enqueue(&queue, 3);
+    assert_true(!queue_is_full(&queue));
+    queue_enqueue(&queue, 4);
+    assert_true(queue_is_full(&queue));
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_increase_capacity)
 {
     queue(char) queue = queue_create();
-    queue_increase_capacity(queue);
+    queue_increase_capacity(&queue);
     assert_true(queue.capacity == 1);
     for (int i = 0; i < 3; ++i) {
-        queue_increase_capacity(queue);
+        queue_increase_capacity(&queue);
     }
     assert_true(queue.capacity == 8);
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_enqueue)
 {
     queue(char) queue = queue_create();
     for (char c = '0'; c <= '9'; ++c) {
-        queue_enqueue(queue, c);
+        queue_enqueue(&queue, c);
     }
     assert_true(queue.size == 10);
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_dequeue)
 {
     queue(char) queue = queue_create();
     for (char c = '0'; c <= '9'; ++c) {
-        queue_enqueue(queue, c);
+        queue_enqueue(&queue, c);
     }
     for (int i = 0; i < 10; ++i) {
-        queue_dequeue(queue);
+        queue_dequeue(&queue);
     }
-    assert_true(queue_is_empty(queue));
-    queue_dequeue(queue);
-    assert_true(queue_is_empty(queue));
-    queue_destroy(queue);
+    assert_true(queue_is_empty(&queue));
+    queue_dequeue(&queue);
+    assert_true(queue_is_empty(&queue));
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_peek)
@@ -81,50 +81,50 @@ unit_test(test_queue_peek)
     queue(char) queue = queue_create();
     char array[] = "0123456789";
     for (int i = 0; i < 10; ++i) {
-        queue_enqueue(queue, array[i]);
+        queue_enqueue(&queue, array[i]);
     }
     for (int i = 0; i < 10; ++i) {
-        assert_true(array[i] == *(queue_peek(queue)));
-        queue_dequeue(queue);
+        assert_true(array[i] == *(queue_peek(&queue)));
+        queue_dequeue(&queue);
     }
-    void *ptr = queue_peek(queue);
+    void *ptr = queue_peek(&queue);
     assert_true(!ptr);
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_destroy)
 {
     queue(char) queue = queue_create();
     for (char c = '0'; c <= '9'; ++c) {
-        queue_enqueue(queue, c);
+        queue_enqueue(&queue, c);
     }
-    queue_destroy(queue);
+    queue_destroy(&queue);
     assert_true(!queue.array);
     assert_true(queue.size == 0);
     assert_true(queue.capacity == 0);
     assert_true(queue.front == 0);
     assert_true(queue.back == 0);
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 unit_test(test_queue_pointer_type)
 {
     queue(const char*) queue = queue_create();
-    queue_enqueue(queue, "hello");
-    queue_enqueue(queue, ", ");
-    queue_enqueue(queue, "world");
-    queue_enqueue(queue, "!\n");
+    queue_enqueue(&queue, "hello");
+    queue_enqueue(&queue, ", ");
+    queue_enqueue(&queue, "world");
+    queue_enqueue(&queue, "!\n");
 
     const char *array[] = {"hello", ", ", "world", "!\n"};
     for (int i = 0; i < 4; ++i) {
-        const char **ptr = queue_peek(queue);
-        queue_dequeue(queue);
+        const char **ptr = queue_peek(&queue);
+        queue_dequeue(&queue);
         assert_true(ptr);
         if (ptr) {
             assert_true(strcmp(*ptr, array[i]) == 0);
         }
     }
-    queue_destroy(queue);
+    queue_destroy(&queue);
 }
 
 int main()
